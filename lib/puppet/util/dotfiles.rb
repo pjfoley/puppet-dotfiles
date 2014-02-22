827e30732524cecc57e894220de9f53cd4898599
Peter Foley <peter@ifoley.id.au>
2014-02-22 21:43:05 +1100

    Functioning Install of dotfiles

--- lib/puppet/util/dotfiles.rb
+++ lib/puppet/util/dotfiles.rb
@@ -3,11 +3,11 @@ require File.expand_path('../dotfiles/dotfile', __FILE__)
 module Puppet
 module Util
   class Dotfiles
-    include Enumerable
+#    include Enumerable
 
-    def initialize(dotfiles, home, backup = false, backup_location = "", removed_dotfiles = [])
+    def initialize(dotfiles, home, backup = true, backup_path = "#{home}/.backupdot", removed_dotfiles = [])
       # Folder locations
-      @dotfiles, @home, @backup_location = dotfiles, home, backup_location
+      @dotfiles_path, @home_path, @backup_path = dotfiles, home, backup_path
 
       # Do we want to backup any existing files
       @backup = backup
@@ -18,10 +18,50 @@ module Util
       # Files to clean up from home directory
       @removed_dotfiles = removed_dotfiles
 
-      if File.directory?(@dotfiles)
-        parse_dotfiles
-      end
+      @dotfile_names = []
+      @dotfile_hash = {}
 
+      @exists = false
+
+      if File.directory?(@dotfiles_path)
+        parse_dotfiles
+        @exists = dotfile_hash.collect { |k, v| v.exists_dotfile?}.all?
+      end
+    end
+
+    attr_reader :dotfile_names, :dotfile_hash, :dotfiles_path, :home_path, :backup_path, :timestamp
+
+
+    def create
+      @dotfile_hash.each do |k,df|
+        df.backup if @backup && df.exists_bkupfile?
+        df.install
+      end
+    end
+
+    def destroy
+      puts "Lets tear the joint down!"
+    end
+
+    def exists?
+      @exists
+    end
+
+
+    private
+
+    def add_dotfile(df)
+      @dotfile_hash[df.homefile] = df
+      @dotfile_names << df.homefile
+    end
+
+    def parse_dotfiles(subdir = nil)
+      directory = subdir.nil? ? @dotfiles_path : subdir
+      Dir.glob(directory + "/*").each do |f|
+        df = Dotfile.new(f, self)
+        add_dotfile(df)
+        parse_dotfiles(f) if df.directory?
+      end
     end
   end
 end
--- lib/puppet/util/dotfiles/dotfile.rb
+++ lib/puppet/util/dotfiles/dotfile.rb
@@ -1,11 +1,101 @@
+require 'fileutils'
+require 'pathname'
+
 module Puppet
 module Util
 class Dotfiles
   class Dotfile
-    def initialize *args
+    attr_reader :dotfile, :homefile, :backupfile
 
-      @home_name = ""
-      
+    def initialize(file, dfs)
+      relative_path = Pathname.new(file).relative_path_from(Pathname.new(dfs.dotfiles_path)) \
+                      .to_path \
+                      # Regex match from start of line and replace
+                      .gsub(/^(dot_|dcp_)/, '.') \
+                      .gsub(/^(cpy_|mkd_)/, '') \
+                      # Regex match from path seperator '/' and replace
+                      .gsub(/(\/dot_|\/dcp_)/, '/.') \
+                      .gsub(/(\/cpy_|\/mkd_)/, '/')
+
+      @timestamp = dfs.timestamp
+
+      @dotfile = file
+      @homefile = (Pathname.new(dfs.home_path) + relative_path).to_path
+      @backupfile = (Pathname.new(dfs.backup_path) + relative_path).to_path
+
+      @action = case File.basename(file)[0,3]
+                  when "dcp", "cpy" then "copy"
+                  when "mkd" then "mkdir"
+                  else "link"
+                end
+
+      @exists_dotfile \
+              = case @action
+                  when "link" then File.identical?(@dotfile, @homefile)
+                  when "copy" then File.exists?(@homefile)
+                  when "mkdir" then File.directory?(@homefile)
+                end
+    end
+
+    def to_s
+      "#{@dotfile}\n\t#{@homefile}\n\t#{@backupfile}\n\t" + (@exists ? "True" : "False")
+    end
+
+    def install
+      if !exists_dotfile?
+        self.send(@action)
+      end
+    end
+
+    def remove
+      puts "Removing"
+    end
+
+    def backup
+      backup_file
+    end
+
+    def directory?
+      @action == 'mkdir' ? true : false
+    end
+
+    def exists_dotfile?
+      @exists_dotfile
+    end
+
+    def exists_bkupfile?
+      return false if ! File.exists?(@homefile) || exists_dotfile?
+      case @action
+        when "copy" then false
+        else
+          bck_fn = Dir.glob(File.dirname(@backupfile) + "/*" + File.basename(@backupfile)) \
+                            .sort_by { |e| e[/\d{14}/].to_i }.last
+
+          return true if ! bck_fn
+
+          puts "Compare me: " + bck_fn.to_s
+
+          ! FileUtils.compare_file(@homefile, bck_fn)
+      end
+    end
+
+    private
+
+    def copy
+      FileUtils.copy @dotfile, @homefile
+    end
+
+    def link
+      FileUtils.ln_sf @dotfile, @homefile
+    end
+
+    def mkdir
+      FileUtils.mkdir_p @homefile
+    end
+
+    def backup_file
+      FileUtils.mkdir_p File.dirname(@backupfile) if ! File.directory?(File.dirname(@backupfile))
+      FileUtils.mv @homefile, File.dirname(@backupfile) + "/#{@timestamp}_" + File.basename(@backupfile)
     end
   end
 end
